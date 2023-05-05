eab4062cf3e9e7ef13455f68780f0cef754ee356
[SW4/1반] 신혜지 <shinhj4068@gmail.com>
2023-05-06 02:03:57 +0900

    feat: state, generic 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,86 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { useState } from 'react';
+import './index.css';
 
-type CounterProps = {
-  title:string,
-  initVlaue?:number
-}
+type counterProps = {
+  title: string;
+  initValue?: number;
+};
 
-function Counter({title, initVlaue}:CounterProps) {
-  let countState = useState(initVlaue); // 배열 return 
-  // 첫번째 원소는 initValue와 같음, 두 번째는 함수
-  // state 첫 번째 원소는 읽을 때 쓰고 두 번째 원소는 변경할 때 쓴다
-  let [count, setCount] = countState;
+function Counter({ title, initValue = 0 }: counterProps) {
+  let [value, setValue] = useState(initValue);
 
   function up() {
-    // initValue의 값을 증가
-    // 값이 증가되면 컴포넌트가 다시 실행되서 새로운 값이 화면에 표시
-    setCount((count === undefined ? 0 : count) + 1)
+    setValue(value + 1);
   }
-  
+
   function down() {
-    setCount((count === undefined ? 0 : count) - 1)
+    setValue(value - 1);
   }
 
   return (
     <div>
       <h1>{title}</h1>
-      <button onClick={up}>+</button>
-      <button onClick={down}>-</button> {count}
-    </div>
-  )
-}
-
-function App() {
-  return (
-    <div>
-      <Counter title="불면증 카운터" initVlaue={0}/>
+      <button onClick={up}>+</button> {value}&nbsp;
+      <button onClick={down}>-</button>
     </div>
   );
 }
 
-export default App;
+function getCurrentTime() {
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+
+  return `${hours}:${minutes}:${seconds}`;
+}
+
+function Counter2() {
+  // 1. state가 배열이나 객체일 때는 값을 변경할 때 복제본을 변경한다.
+  // 2. for, map을 이용해 목록을 생성할 때 key 값을 재공한다.
+  // 3. state가 배열일 때 값이 없다면 추론을 사용할 수 없기 때문에 제네릭을 명시적으로 입력한다.
+  const [times, setTimes] = useState<string[]>([]);
+  function up() {
+    const newTimes = [...times];
+    newTimes.push(getCurrentTime());
+    setTimes(newTimes);
+  }
+
+  return (
+    <div>
+      <h1>시간 찍기 카운터</h1>
+      <button onClick={up}>⏰</button>
+      <ol>{times.map((value, index) => {
+        return <li key={index}>{value}</li>
+      })}</ol>
+    </div>
+  );
+}
+
+function Counter3() {
+  // input과 같은 form 태그와 state를 연동해서 react에서 사용하는 방법
+  const [count, setCount] = useState<number>(0);
+  const [step, setStep] = useState<number>(1);
+
+  return (
+    <div>
+      <h1>Counter3</h1>
+      <input type="number" defaultValue={step} onChange={(event) => {
+        setStep(Number(event.target.value))
+      }}/>
+      <button onClick={() => setCount(count + step)}>+</button>{count}
+    </div>
+  )
+}
+
+export default function App() {
+  return (
+    <div>
+      <Counter title="카운터" initValue={0} />
+      <Counter title="불면증 카운터" />
+      <Counter2 />
+      <Counter3 />
+    </div>
+  );
+}
