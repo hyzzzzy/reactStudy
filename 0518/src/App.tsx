dfe9eb67d119541682d15a704110fd75d146b7e8
hyzzzzy <shinhj4068@naver.com>
2023-05-18 10:57:32 +0900

    feat: redux-toolkit 사용

--- 0518/src/App.tsx
+++ 0518/src/App.tsx
@@ -1,51 +1,55 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 import React, { useState } from "react";
-
-type Counter1Props = {
-  value: number;
-  onChange: (value: number) => void;
+import { Provider, useDispatch, useSelector } from "react-redux";
+const counterInitialState = {
+  value: 10,
 };
-
-function Counter1({ value, onChange }: Counter1Props) {
+const counterReducers = {
+  UP: (state: any) => {
+    state.value = state.value + 1;
+  },
+};
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: counterInitialState,
+  reducers: counterReducers,
+});
+const store = configureStore({
+  reducer: {
+    counter: counterSlice.reducer,
+  },
+});
+function Counter1() {
+  const count = useSelector((state: any) => state.counter.value);
+  const dispatch = useDispatch();
   return (
     <div>
       <h1>Counter</h1>
-      {/* 부모 컴포넌트의 상태를 바꾸기 위해서는 
-      자식 컴포넌트로 함수를 props로 전달하고, 
-      자식 컴포넌트는 적당한 타이밍에 그 함수를 호출한다. 
-      호출된 함수가 state를 바꾼다. */}
       <button
         onClick={() => {
-          onChange(value + 1);
+          dispatch(counterSlice.actions.UP());
         }}
       >
         +
       </button>
-      {value}
+      {count}
     </div>
   );
 }
-
-type Counter2Props = {
-  value: number;
-};
-
-function Counter2({ value }: Counter2Props) {
-  return <div>{value}</div>;
+function Counter2() {
+  const count = useSelector((state: any) => state.counter.value);
+  return <div>{count}</div>;
 }
-
 function App() {
-  const [count, setCount] = useState(10);
   return (
-    <div>
-      <Counter1
-        value={count}
-        onChange={(value) => {
-          setCount(value + 1);
-        }}
-      ></Counter1>
-      <Counter2 value={count}></Counter2>
-    </div>
+    <Provider store={store}>
+      <div>
+        <Counter1></Counter1>
+        <Counter2></Counter2>
+      </div>
+    </Provider>
   );
 }
+
 
 export default App;
