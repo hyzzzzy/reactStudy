6a26fba3a163e58c54f618100fbbf2398a6bf879
hyzzzzy <shinhj4068@naver.com>
2023-05-18 11:38:27 +0900

    merge: 0511 Repo

--- 0518/src/App.tsx
+++ 0518/src/App.tsx
@@ -1,30 +1,48 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, configureStore, createSlice } from "@reduxjs/toolkit";
 import React, { useState } from "react";
 import { Provider, useDispatch, useSelector } from "react-redux";
+
 const counterInitialState = {
-  value: 10,
+  value: 11,
+  step: 2,
 };
+
 const counterReducers = {
   UP: (state: any) => {
     state.value = state.value + 1;
   },
+  STEP: (state: any, action: PayloadAction<number>) => {
+    state.step = action.payload;
+  },
 };
+
 const counterSlice = createSlice({
   name: "counter",
   initialState: counterInitialState,
   reducers: counterReducers,
 });
+
 const store = configureStore({
   reducer: {
     counter: counterSlice.reducer,
   },
 });
+
 function Counter1() {
   const count = useSelector((state: any) => state.counter.value);
+  const step = useSelector((state: any) => state.counter.step);
   const dispatch = useDispatch();
   return (
     <div>
       <h1>Counter</h1>
+      <input
+        type="number"
+        value={step}
+        onChange={(evt) => {
+          const step = Number(evt.target.value);
+          dispatch(counterSlice.actions.STEP(step));
+        }}
+      />
       <button
         onClick={() => {
           dispatch(counterSlice.actions.UP());
@@ -36,10 +54,12 @@ function Counter1() {
     </div>
   );
 }
+
 function Counter2() {
   const count = useSelector((state: any) => state.counter.value);
   return <div>{count}</div>;
 }
+
 function App() {
   return (
     <Provider store={store}>
