8b1a94f699badff60df527f3e8cd31d2117b4100
hyzzzzy <shinhj4068@naver.com>
2023-05-23 13:23:56 +0900

    design: 컴포넌트 구현

---
+++ 0523/components/Layout.tsx
@@ -0,0 +1,36 @@
+export default function Layout() {
+  return (
+    <>
+      <header>
+        <h1>
+          <a href="/">WEB</a>
+        </h1>
+      </header>
+      <nav>
+        <ol>
+          <li>
+            <a href="/read/1">html</a>
+          </li>
+          <li>
+            <a href="/read/2">css</a>
+          </li>
+        </ol>
+      </nav>
+      <article>
+        <h2>Welcome</h2>
+        Hello, World!
+      </article>
+      <ul>
+        <li>
+          <a href="/create">Create</a>
+        </li>
+        <li>
+          <a href="/update">Update</a>
+        </li>
+        <li>
+          <a href="/delete">Delete</a>
+        </li>
+      </ul>
+    </>
+  );
+}
--- 0523/pages/index.tsx
+++ 0523/pages/index.tsx
@@ -1,3 +1,4 @@
+import Layout from "@/components/Layout";
 import Head from "next/head";
 
 export default function Home() {
@@ -9,21 +10,7 @@ export default function Home() {
       <header>
         <h1><a href="/">WEB</a></h1>
       </header>
-      <nav>
-        <ol>
-          <li><a href="/read/1">html</a></li>
-          <li><a href="/read/2">css</a></li>
-        </ol>
-      </nav>
-      <article>
-        <h2>Welcome</h2>
-        Hello, World!
-      </article>
-      <ul>
-        <li><a href="/create">Create</a></li>
-        <li><a href="/update">Update</a></li>
-        <li><a href="/delete">Delete</a></li>
-      </ul>
+      <Layout></Layout>
     </>
   );
 }
