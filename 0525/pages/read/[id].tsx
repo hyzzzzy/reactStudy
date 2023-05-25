c70da5af86b36d80b0e90c3c855ad0395120f8cd
hyzzzzy <shinhj4068@naver.com>
2023-05-25 11:44:36 +0900

    feat: 읽기 기능 구현

--- 0525/pages/read/[id].tsx
+++ 0525/pages/read/[id].tsx
@@ -1,14 +1,36 @@
 import Layout from "@/components/Layout";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function Read() {
+  const router = useRouter();
+  const [topic, setTopic] = useState(null);
+  const id = router.query.id;
+  console.log(router.query.id);
+  //debugger;
+  useEffect(() => {
+    if (id !== undefined) {
+      fetch('/api/topics/' + id)
+        .then((resp) => resp.json())
+        .then((result) => {
+          console.log(result);
+          setTopic(result);
+        });
+    }
+  }, [id]);
+
+  if (topic === null) {
+    return <>Loading...</>
+  }
+
   return (
     <>
       <Head>
         <title>WEB!</title>
       </Head>
-      <h2>Read!</h2>
-      Hello, Read!
+      <h2>{topic.title}</h2>
+      {topic.body}
     </>
   );
 }
